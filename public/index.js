28392708b09f44dfe1c1f8aac505b79a983d1dc1
MaxGolubev-CloudFM <m.golubev@cloudfmgroup.com>
2017-11-13 11:33:41 +0000

    changing adderss to  https://murmuring-brushlands-33347.herokuapp.com/

--- public/index.js
+++ public/index.js
@@ -2,4 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 
-ReactDOM.render(<App url="http://localhost:8000/api/data" />, document.querySelector('.container'));
+// ReactDOM.render(<App url="http://localhost:8000/api/data" />, document.querySelector('.container'));
+ReactDOM.render(<App url="https://murmuring-brushlands-33347.herokuapp.com/api/data" />, document.querySelector('.container'));
