020c276a05700a280281648df994f8cf44bf722f
MaxGolubev-CloudFM <m.golubev@cloudfmgroup.com>
2017-11-13 15:57:48 +0000

    heroku image extension to lowercase

--- public/index.js
+++ public/index.js
@@ -4,3 +4,5 @@ import App from './app';
 
 // ReactDOM.render(<App url="http://localhost:8000/api/data" />, document.querySelector('.container'));
 ReactDOM.render(<App url="https://murmuring-brushlands-33347.herokuapp.com/api/data" />, document.querySelector('.container'));
+
+// 
