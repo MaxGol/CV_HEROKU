5f7cf36bf49e72dc3542aa835eccf9a5b58f2a56
MaxGolubev-CloudFM <m.golubev@cloudfmgroup.com>
2017-11-21 16:00:44 +0000

    changing new name, upload to heroku

--- public/index.js
+++ public/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './app';
 
 // ReactDOM.render(<App url="http://localhost:8000/api/data" />, document.querySelector('.container'));
-ReactDOM.render(<App url="https://murmuring-brushlands-33347.herokuapp.com/api/data" />, document.querySelector('.container'));
+ReactDOM.render(<App url="https://maxgolubev-cv.herokuapp.com/api/data" />, document.querySelector('.container'));
 
 //
